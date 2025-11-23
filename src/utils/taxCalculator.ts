e556e0396782e9ded177b9ebcb98298705940dc5
4drea4 <bakerand@outlook.com>
2025-11-22 18:02:44 -0600

    working on tax calculator have one error for be taxed

--- src/utils/taxCalculator.ts
+++ src/utils/taxCalculator.ts
@@ -3,14 +3,13 @@
 // Note that the product data returned from the API does not include a taxPercentage field like it includes a discountPercentage field. Apply a default standard tax rate of 4.75% to each product; however, products with a category of “groceries” should only be taxed at 3%.
 
 import { Product } from "../models/Product.js"
-import { fetchProducts } from  "../services/apiService.js" ;
 
 // Create a calculateTax() function to handle tax calculations for products.
 
 export function calculateTax(product: Product):number { //take in a product object from api 
 
  //see what tax to use
- let beTaxed = (number:number);
+ let beTaxed :number;
 
  if(product.category === "groceries" ){
    beTaxed = product.price * (3/100);
@@ -23,7 +22,7 @@ return(beTaxed);
   
 
 
-}
+} 
 
 
 
